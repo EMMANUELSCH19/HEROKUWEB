ecd34256a5f964969242aa612774f48e77d9429f
EMMANUELSCH19 <emmanuel_1320114041@uptecamac.edu.mx>
2021-11-10 16:50:45 -0600

    a

--- correo.php
+++ correo.php
@@ -2,6 +2,7 @@
 
     //CORREO AL QUE SE ENVIARA EL MENSAJE
     $destinatario = 'emmanuel_1320114041@uptecamac.edu.mx';
+    $asunto = 'Asunto de prueba';
     $nombre = $_POST['nombre'];
     $correo = $_POST['correo'];
     $apellido = $_POST['apellido'];
@@ -15,7 +16,7 @@
     $header = "Enviado desde la pagina BELLALOVERS";
     $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre . "\n" . $apellido;
 
-    mail($destinatario, $mensajeCompleto, $header);
+    mail($destinatario, $asunto, $mensajeCompleto, $header);
 
     echo "<script>alert('correo enviado exitosamente')</script>";
     echo "<script>setTimeout(\"location.href='index.php'\",1000)</script>";
